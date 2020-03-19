82d99c3d572d123bf3951d59d0f0dfb15bccd2da
sy.moon <xlrj0716@gmail.com>
2020-03-19 21:50:38 +0900

    menu

--- src/components/blog/Menu.vue
+++ src/components/blog/Menu.vue
@@ -2,7 +2,7 @@
     <div class="menu">
         <button
             type="button"
-            class="menu__button"
+            :class="['menu__button', selectMenu === 'all' ? 'is-active' : '']"
             value="all"
             @click="secSelect"
         >
@@ -10,7 +10,10 @@
         </button>
         <button
             type="button"
-            class="menu__button"
+            :class="[
+                'menu__button',
+                selectMenu === 'review' ? 'is-active' : '',
+            ]"
             value="review"
             @click="secSelect"
         >
@@ -18,29 +21,60 @@
         </button>
         <button
             type="button"
-            class="menu__button"
+            :class="['menu__button', selectMenu === 'study' ? 'is-active' : '']"
             value="study"
             @click="secSelect"
         >
             <span>.study</span>
         </button>
-        <div class="menu__line" aria-hidden="true"></div>
+        <div
+            class="menu__line"
+            aria-hidden="true"
+            ref="line"
+            v-show="selectMenu"
+        ></div>
     </div>
 </template>
 
 <script>
 export default {
     porps: ['sec'],
+    data() {
+        return {
+            selectMenu: 'all',
+        };
+    },
     methods: {
-        secSelect(e) {
-            this.$emit('secUpdate', e.currentTarget.value);
+        lineBar($el) {
+            const { offsetLeft, clientWidth } = $el;
+            const $line = this.$refs.line;
+            $line.style.left = `${offsetLeft}px`;
+            $line.style.width = `${clientWidth}px`;
         },
+        secSelect(e) {
+            const $target = e.currentTarget;
+            this.lineBar($target);
+            this.selectMenu = $target.value;
+            this.$emit('secUpdate', this.selectMenu);
+        },
+    },
+
+    mounted() {
+        this.$nextTick(function() {
+            setTimeout(() => {
+                const $el = document.getElementsByClassName('is-active');
+                this.lineBar($el[0]);
+            }, 200);
+
+            // 모든 화면이 렌더링된 후 실행합니다.
+        });
     },
 };
 </script>
 
 <style scoped lang="scss">
 .menu {
+    position: relative;
     display: table;
     &__button {
         display: inline-block;
@@ -54,12 +88,22 @@ export default {
             padding: 0 6px;
             font-weight: 500;
             color: $color1;
+            transition: color 0.5s ease-out;
+        }
+        &.is-active {
+            span {
+                color: $color8;
+            }
         }
     }
     &__line {
+        position: absolute;
+        left: 0;
+        bottom: -5px;
+        width: 0;
         height: 2px;
-        margin-top: 10px;
         background-color: $color1;
+        transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
     }
 }
 </style>
