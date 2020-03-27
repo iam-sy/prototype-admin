17f6d9c7721252abfb867d03fd6eb0dd0af9f394
sy.moon <xlrj0716@gmail.com>
2020-03-27 16:24:03 +0900

    error 처리

--- src/utils/extensions/axios.js
+++ src/utils/extensions/axios.js
@@ -4,13 +4,25 @@ import constants from '../../../common/config';
 const instance = axios.create({
     baseURL: constants.apiAdress,
 });
-instance.interceptors.request.use(conf => {
-    eventBus.$emit('start:spinner');
-    return conf;
-});
-instance.interceptors.response.use(response => {
-    eventBus.$emit('end:spinner');
-    return response;
-});
+instance.interceptors.request.use(
+    conf => {
+        eventBus.$emit('start:spinner');
+        return conf;
+    },
+    error => {
+        eventBus.$emit('end:spinner');
+        return Promise.reject(error);
+    },
+);
+instance.interceptors.response.use(
+    response => {
+        eventBus.$emit('end:spinner');
+        return response;
+    },
+    error => {
+        eventBus.$emit('end:spinner');
+        return Promise.reject(error);
+    },
+);
 
 export default instance;
